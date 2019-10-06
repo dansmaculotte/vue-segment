ab655ce325abbf9f54d5a16ff99faec424c970b3
Gaël Reyrol <me@gaelreyrol.com>
2019-10-06 11:15:21 +0200

    test: add vue-router to allow router afterEach hook test

--- test/fixture/App.vue
+++ test/fixture/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <nav>
+      <ul class="header-ul">
+        <li>
+          <router-link class="router-link" to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link class="router-link" to="/about">About</router-link>
+        </li>
+        <li>
+          <router-link class="router-link" to="/contact">Contact</router-link>
+        </li>
+      </ul>
+    </nav>
+    <router-view></router-view>
     <h1>Works !</h1>
     <button @click="load">load segment</button>
   </div>
---
+++ test/fixture/pages/Contact.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact'
+}
+</script>
